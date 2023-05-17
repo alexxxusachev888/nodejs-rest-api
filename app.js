e6f4b7c8e6ccde7d15cf95808346993485bf1088
alexxx888 <alexxxusachev888@gmail.com>
2023-05-17 14:13:12 +0300

    mongoose module has been implemented, added modes, schema, patch route for favorite value

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts');
+require('dotenv').config();
 
 const app = express()
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getAllContacts,
+    getContactById,
+    addNewContact,
+    updateContact,
+    updateFavorite,
+    deleteContact
+  } = require('../../controllers/contacts');
+  
+const { schemas: {contactSchemaJoi, updateFavoriteSchemaJoi} } = require('../../models/contacts');
+const isValidId = require('../../middlewares/isValidId')
+const {validateBody, validateFavorites} = require('../../middlewares/validateBody');
 
-const controls = require('../../controllers/contacts');
-const schema = require('../../schemas/contacts');
-const validate = require('../../middlewares/validateBody');
+router.get('/', getAllContacts);
 
-router.get('/', controls.getAllContacts)
+router.get('/:id', isValidId, getContactById);
 
-router.get('/:id', controls.getContactById)
+router.post('/', validateBody(contactSchemaJoi), addNewContact);
 
-router.post('/', validate(schema), controls.addNewContact)
+router.delete('/:id', isValidId, deleteContact);
 
-router.delete('/:id', controls.deleteContact)
+router.put('/:id', isValidId, validateBody(contactSchemaJoi), updateContact) ;
 
-router.put('/:id', validate(schema), controls.updateContact)
+router.patch('/:id/favorite', isValidId, validateFavorites(updateFavoriteSchemaJoi), updateFavorite);
 
-module.exports = router
+module.exports = router;
