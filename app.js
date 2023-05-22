68f448998dd2abf84d529d09f464f9c93f3dcaf8
alexxx888 <alexxxusachev888@gmail.com>
2023-05-22 20:35:02 +0300

    file save implemented

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 const contactsRouter = require('./routes/api/contacts');
 const authRouter = require('./routes/api/auth');
 
-
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'))
 
 app.use('/users', authRouter);
 app.use('/api/contacts', contactsRouter);
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
 const authenticate = require('./authenticate.js');
 const isValidId = require('./isValidId.js');
-const {validateBody, validateFavorites} = require('./validateBody.js')
+const {validateBody, validateFavorites} = require('./validateBody.js');
+const upload = require('./upload');
 
 module.exports = {
     authenticate,
     isValidId,
     validateBody,
-    validateFavorites
+    validateFavorites,
+    upload,
 }
